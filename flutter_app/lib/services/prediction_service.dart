4a51c15b5c4af544704fa3fc9128b58b4c68af51
John Paul Tiu <johnpaul965@gmail.com>
2026-09-14 22:51:40 +0800

    Update metrics and web styles

--- flutter_app/lib/services/prediction_service.dart
+++ flutter_app/lib/services/prediction_service.dart
@@ -1,8 +1,6 @@
 import 'dart:convert';
-import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import '../config.dart';
 import '../models.dart';
 
 class PredictionResponse {
